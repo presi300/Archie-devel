2b5af55f8f5873ce573395bf1ee367f469455073
presi300 <50592132+presi300@users.noreply.github.com>
2021-12-11 11:18:48 +0200

    Delete Subscripts directory

--- Subscripts/Auto/DiskFormat/uefi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-dsk=$(cat /home/presi300/Documents/Archie/Temp_Files/userdisk.txt)
-function spacer {
-echo "-------------------------------------------------------------------"
-}
-clear
-
-
