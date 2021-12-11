ff05f0bcd78dd89bb097182726c48e76372f1c8c
presi300 <50592132+presi300@users.noreply.github.com>
2021-12-11 11:11:34 +0200

    lmao

---
+++ Subscripts/Auto/DiskFormat/uefi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+dsk=$(cat /home/presi300/Documents/Archie/Temp_Files/userdisk.txt)
+function spacer {
+echo "-------------------------------------------------------------------"
+}
+clear
+
+
