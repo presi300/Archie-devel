72e178a4d58501b584838b554cb36a638ab0a275
presi300 <50592132+presi300@users.noreply.github.com>
2021-12-12 10:42:22 +0200

    Add files via upload

---
+++ Subscripts/Auto/DiskFormat/mbr.sh
@@ -0,0 +1,12 @@
+ #!/usr/bin/env bash
+dsk=$(cat Temp_Files/userdisk.txt)
+function spacer {
+echo "-------------------------------------------------------------------"
+}
+
+touch sfdisk.txt
+wipefs -a $dsk
+echo "label: DOS" >> sfdisk.txt
+echo ",4G, S" >> sfdisk.txt
+echo ",, L" >> sfdisk.txt
+sfdisk $dsk < sfdisk.txt
